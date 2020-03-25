bf23c38722228ebbf3990974dfdb665d840272c2
tlaskey <tlaskey3@gmail.com>
2020-03-24 19:14:22 -0700

    grab player hiscores and return category user specifies

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const GrandExchange = require('./lib/Grand Exchange/items_api')
+const Hiscores = require('./lib/Hiscores/highscores_api')
 
 async function get_ge_item(item_id) {
     const ge = new GrandExchange()
@@ -8,4 +9,10 @@ async function get_ge_item(item_id) {
     console.log(response)
 }
 
-get_ge_item(2)
+async function get_player_hiscores(username, game_mode, category) {
+    const hiscores = new Hiscores()
+    const responseBody = await hiscores.get_player(username, game_mode)
+    console.log(hiscores.parse_hiscores(responseBody, category))
+}
+
+get_player_hiscores('Manr', 'main', 'overall')

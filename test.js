77e79d54a51eec86e28cf8cf4bc28aa70021ea68
tlaskey <tlaskey3@gmail.com>
2020-03-24 18:43:17 -0700

    initial commit

--- test.js
+++ test.js
@@ -5,8 +5,7 @@ const GrandExchange = require('./lib/Grand Exchange/items_api')
 async function get_ge_item(item_id) {
     const ge = new GrandExchange()
     const response = await ge.get_item(item_id)
-    console.log(response.statusCode)
-    console.log(response.body)
+    console.log(response)
 }
 
 get_ge_item(2)

f07d2c0ddbc6a965c1b513b99b182ca6b585095a
tlaskey <tlaskey3@gmail.com>
2020-03-25 15:28:42 -0700

    add rudimentary parsing of response body

--- sample.js
+++ sample.js
@@ -1,20 +1,20 @@
 'use strict'
 
-const GrandExchange = require('./lib/Grand Exchange/items_api')
-const Hiscores = require('./lib/Hiscores/highscores_api')
+const GrandExchange = new (require('./lib/Grand Exchange/items_api'))()
+const Hiscores = new (require('./lib/Hiscores/highscores_api'))()
 
 async function getItem (itemID) {
-  const ge = new GrandExchange()
-  const response = await ge.getItem(itemID)
-  console.log(itemID, response.body)
-  console.log(ge.parseItemBody(response.body))
+  const responseBody = await GrandExchange.getItem(itemID)
+  console.log(GrandExchange.parseItemBody(responseBody))
+  console.log(responseBody)
+  return responseBody
 }
 
 async function getPlayerHiscores (username, gameMode, category) {
-  const hiscores = new Hiscores()
-  const responseBody = await hiscores.getPlayer(username, gameMode)
-  console.log(hiscores.parseHiscores(responseBody, category))
+  const responseBody = await Hiscores.getPlayer(username, gameMode)
+  console.log(Hiscores.parseHiscores(username, responseBody, category))
+  return responseBody
 }
 
 getItem(2)
-getPlayerHiscores('Manr', 'main', 'overall')
+getPlayerHiscores('Manr', 'main', 'strength')
